795471019917197d6b501e5b925e9dada46d8fe0
kevinkim910408 <junho2413@gmail.com>
2023-04-14 15:36:13 -0400

    feat: input & 반응형

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -1,17 +1,29 @@
+"use client";
+
+import { Input } from "@/components/_Materials/Input";
+import { useCallback } from "react";
+
 const Page = () => {
+  const onChangeId = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {},
+  []);
   return (
-    <div className="relative flex flex-col items-center gap-12 min-h-screen overflow-hidden">
+    <div className="relative flex flex-col items-center gap-12 min-h-screen overflow-hidden sm:gap-4 xsm:gap-2">
       {/* back button => component with actual link*/}
 
-      <div className="font-semibold mt-8">게시글작성</div>
+      <div className="font-bold mt-8 text-lg xsm:mt-4">게시글작성</div>
 
-      <div className="w-[calc(100%-4rem)] p-10 flex flex-col gap-4 items-center bg-[#DF7272] rounded-lg">
+      <div className="w-[calc(100%-4rem)] p-10 flex flex-col gap-4 items-center bg-[#DF7272] rounded-3xl sm:p-5 xsm:p-3">
         <div className="flex flex-col w-full">
-          <span className="text-white">글작성</span>
-          <input type="text" placeholder="something" />
+          <span>글작성</span>
+          <Input
+            placeholder="ddd"
+            type="text"
+            onChange={onChangeId}
+            height="h-11"
+          />
         </div>
         <div className="flex flex-col w-full">
-          <span className="text-white">카테고리</span>
+          <span>카테고리</span>
           <select name="" id="">
             <option>dd</option>
             <option>dd</option>
@@ -20,23 +32,50 @@ const Page = () => {
         </div>
       </div>
 
-      <div className="w-[calc(100%-4rem)] p-10 flex flex-col gap-4 bg-[#DF7272] rounded-lg">
-        <span className="text-white">선택지 작성</span>
+      <div className="w-[calc(100%-4rem)] p-10 flex flex-col gap-4 bg-[#DF7272] rounded-3xl sm:p-5 xsm:p-3">
+        <span className="text-lg">선택지 작성</span>
         <div className="flex flex-col w-full">
-          <span className="text-white">선택지 1</span>
-          <input type="text" placeholder="something" />
-          <input type="file" />
+          <span>선택지 1</span>
+          <Input
+            placeholder="ddd"
+            type="text"
+            onChange={onChangeId}
+            height="h-11"
+            round="rounded-t-lg"
+            border="border-none"
+          />
+          <label className="bg-white py-2 px-4 rounded-b-lg  cursor-pointer hover:bg-gray-100  text-xs">
+            이미지 첨부(선택)
+            <input type="file" className="hidden" />
+          </label>
         </div>
         <div className="flex flex-col w-full">
-          <span className="text-white">선택지 2</span>
-          <input type="text" placeholder="something" />
-          <input type="file" />
+          <span>선택지 2</span>
+          <Input
+            placeholder="ddd"
+            type="text"
+            onChange={onChangeId}
+            height="h-11"
+            round="rounded-t-lg"
+            border="border-none"
+          />
+          <label className="bg-white py-2 px-4 rounded-b-lg  cursor-pointer hover:bg-gray-100 text-xs">
+            이미지 첨부(선택)
+            <input type="file" className="hidden" />
+          </label>
         </div>
       </div>
 
-      <button className="w-[calc(100%-4rem)] h-14 flex items-center justify-center bg-white border border-[#DF7272] rounded-md mb-[70px] text-[#DF7272] font-semibold">
+      <button className="w-[calc(100%-4rem)] h-14 flex items-center justify-center bg-white border-2 border-[#DF7272] rounded-md mb-[70px] text-[#DF7272] font-semibold xsm:h-8">
         게시하기
       </button>
+      <style jsx>{`
+        span {
+          color: white;
+          font-weight: bold;
+          margin-bottom: 0.5rem;
+        }
+      `}</style>
     </div>
   );
 };
