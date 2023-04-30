38c7542a5bfc605e79425d7ae8550a9bce4db0aa
kevinkim910408 <junho2413@gmail.com>
2023-04-30 08:44:28 -0400

    feat: dot 메뉴 컴포넌트

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import BackBtnComponent from "@/components/Common/BackBtnComponent";
+import DotMenuComponent from "@/components/Common/DotMenuComponent";
 
 const Page = () => {
   return (
     <div className="relative flex flex-col items-center gap-6 min-h-screen overflow-hidden sm:gap-4">
       <BackBtnComponent red={false} />
+      {/* Profile */}
+      <div className="flex flex-col items-center gap-3 mt-7">
+        <div className="w-10 h-10 bg-[#D9D9D9] rounded-full" />
+        <p className="text-sm font-bold">톡톡톡</p>
+      </div>
+      <DotMenuComponent />
     </div>
   );
 };
