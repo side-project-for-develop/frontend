02f183f85fa7ba585ce82eac68045876f018fb79
kevinkim910408 <junho2413@gmail.com>
2023-04-30 08:17:41 -0400

    feat: 네비게이션 to detail slug

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Page = () => {
+  return (
+    <div className="relative flex flex-col items-center gap-6 min-h-screen overflow-hidden sm:gap-4">
+      dd
+    </div>
+  );
+};
+
+export default Page;
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,6 +4,7 @@ import Logo_H_W from "@/assets/logo_horizontal_white.svg";
 import AddPostComponent from "@/components/Common/AddPostComponent";
 import SearchIcon from "@/assets/search.svg";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 type Category = "전체" | "건강" | "IT" | "유머" | "취미";
 type Sorting = "조회순" | "최신순";
@@ -11,6 +12,7 @@ const categories: Category[] = ["전체", "건강", "IT", "유머", "취미"];
 const sorts: Sorting[] = ["조회순", "최신순"];
 
 const Page = () => {
+  const router = useRouter();
   const [selectedCategory, setSelectedCategory] = useState<string>("전체");
   const [selectedSort, setSelectedSort] = useState<string>("조회순");
   const renderFunction = (value: string, type: "category" | "sort") => {
@@ -38,6 +40,10 @@ const Page = () => {
         {value}
       </div>
     );
+  };
+
+  const onDetailNavigator = () => {
+    router.push("/posts/5");
   };
 
   return (
@@ -75,196 +81,10 @@ const Page = () => {
       {/* Cards */}
       <div className=" w-[100%] flex flex-col items-center mb-[80px]">
         {/* Card */}
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
-          {/* Category */}
-          <div className="mx-4 mt-4">
-            <span className="bg-[#E37070] text-white px-2 rounded-xl ">
-              유머
-            </span>
-          </div>
-          {/* Title */}
-          <div className="text-sm font-bold mx-4 mt-2 mb-1">
-            만약에 내 애인이 내 친구랑 몰래 바람피다 걸린다면?
-          </div>
-          {/* Subtitle */}
-          <div className="text-xs text-[#918d8d] font-bold mx-4">
-            죽인다 / 한번쯤 용서한다
-          </div>
-          {/* User Profile, Name         How many people joined */}
-          <div className="flex justify-between mx-4 mt-2">
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#B8B8B8] w-5 h-5 rounded-full" />
-              <div className="text-xs">hanghae99</div>
-            </div>
-            <div className="flex gap-1 items-center">
-              <div className="bg-[#6e6d6d] w-5 h-5 rounded-full" />
-              <div className="text-[10px] text-[#B8B8B8]">14명 참여</div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer">
+        <div
+          className="w-[90%] flex flex-col w-30 border border-[#eceaea] rounded-2xl mt-3 shadow-postBox pb-3 cursor-pointer"
+          onClick={onDetailNavigator}
+        >
           {/* Category */}
           <div className="mx-4 mt-4">
             <span className="bg-[#E37070] text-white px-2 rounded-xl ">
